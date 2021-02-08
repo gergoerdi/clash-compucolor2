7e5cb03e8bac574fa0a39938c09b7eea8dc77f60
Dr. ERDI Gergo <gergo@erdi.hu>
2021-02-08 20:17:41 +0800

    Create disk image bitfile

--- Shakefile.hs
+++ Shakefile.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards, ViewPatterns, DataKinds #-}
 import Clash.Shake
 import Clash.Shake.Xilinx
 
 import Development.Shake
 import Development.Shake.FilePath
 import Control.Monad
+import Data.List
+import Data.List.Split
+import Clash.Prelude (Bit, BitVector, Unsigned, pack, (!))
+import Numeric (readHex)
 
 targets =
     [ ("nexys-a7-50t", xilinxVivado nexysA750T)
@@ -24,6 +28,13 @@ main = shakeArgs shakeOptions{ shakeFiles = outDir } $ do
 
     let roms = ["chargen.uf6", "v678.rom"]
 
+    outDir </> "disk/*.track" %> \out -> do
+        let i = read $ takeBaseName out
+
+        let inp = "image/disks/hangman.ccvf" -- TODO
+        tracks <- map trackBits . splitTracks <$> readFile' inp
+        writeFileChanged out $ unlines . map show $ tracks !! i
+
     outDir </> "*.bin" %> \out -> do
         let inp = "image" </> dropExtension (takeFileName out)
         binImage Nothing inp out
@@ -33,8 +44,9 @@ main = shakeArgs shakeOptions{ shakeFiles = outDir } $ do
         "Hardware.Compucolor2"
         [ "-Wno-partial-type-signatures"
         -- , "-fclash-inline-limit=600"
-        ] $
+        ] $ do
         need [outDir </> rom <.> "bin" | rom <- roms]
+        need [outDir </> "disk/01.track"] -- TODO
 
     forM_ targets $ \(name, synth) -> do
         SynthKit{..} <- synth kit (outDir </> name </> "synth") ("target" </> name) "Compucolor2"
@@ -45,3 +57,25 @@ main = shakeArgs shakeOptions{ shakeFiles = outDir } $ do
 
     phony "clashi" $
       clash ["--interactive", "src/Hardware/Compucolor2.hs"]
+
+splitTracks :: String -> [String]
+splitTracks =
+    tracks .
+    split (dropInitBlank $ whenElt ("Track" `isPrefixOf`)) .
+    dropWhile (not . ("Track" `isPrefixOf`)) .
+    lines
+  where
+    tracks ([t]:bs:xs) = trackHeader t `seq` mconcat bs : tracks xs
+    tracks [] = []
+
+    trackHeader :: String -> Int
+    trackHeader s = maybe (error $ unwords ["Invalid track header:", show s]) read $ stripPrefix "Track " s
+
+trackBits :: String -> [Bit]
+trackBits = concatBits . map (pack . byte) . chunksOf 2
+  where
+    byte :: String -> Unsigned 8
+    byte s = case readHex s of [(x, "")] -> x
+
+    concatBits :: [BitVector 8] -> [Bit]
+    concatBits = concatMap $ \bv -> [bv!0, bv!1, bv!2, bv!3, bv!4, bv!5, bv!6, bv!7]
