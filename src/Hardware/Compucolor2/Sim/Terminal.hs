6184e6f88364ecc706e99b139cb839680566c2ca
Dr. ERDI Gergo <gergo@erdi.hu>
2021-02-27 14:50:10 +0800

    Remove useless freezing

--- src/Hardware/Compucolor2/Sim/Terminal.hs
+++ src/Hardware/Compucolor2/Sim/Terminal.hs
@@ -9,7 +9,6 @@ import Hardware.Compucolor2.Video
 import Hardware.Compucolor2.CRT5027
 
 import Data.Array.IO
-import Data.Array ((!))
 import Control.Monad
 import Data.Foldable (for_)
 import Control.Monad.Trans
@@ -20,20 +19,23 @@ putScreen
     => IOArray VidAddr (Unsigned 8)
     -> m ()
 putScreen vidRAM = do
-    vidRAM <- liftIO $ freeze vidRAM
     for_ [minBound..maxBound] $ \(y :: Index TextHeight) -> do
         setCursorPosition $ Position (fromIntegral y) 0
 
         for_ [minBound..maxBound] $ \(x :: Index TextWidth) -> do
             let addr = bitCoerce (y, x, (0 :: Index 2))
-                (tall, c) = bitCoerce (vidRAM ! addr) :: (Bool, Unsigned 7)
-                attr = vidRAM ! (addr + 1)
-                (isChar, blink, back, fore) = bitCoerce @_ @(Bool, Bool, _, _) attr
+            b0 <- liftIO $ readArray vidRAM addr
+            b1 <- liftIO $ readArray vidRAM (addr + 1)
+            let (tall, c) = bitCoerce b0
+                (isChar, blink, back, fore) = bitCoerce @_ @(Bool, Bool, _, _) b1
             setAttribute $ foreground $ bright $ toColor fore
             setAttribute $ background $ bright $ toColor back
-            unless (tall && odd y) $ putChar (chr . fromIntegral $ c)
+            putCharCC $ if tall && odd y then 0x20 else c
     flush
   where
+    putCharCC :: Unsigned 7 -> m ()
+    putCharCC = putChar . chr . fromIntegral
+
     toColor :: (Bit, Bit, Bit) -> Color m
     toColor (0, 0, 0) = black
     toColor (0, 0, 1) = red
